2b3e6319e5dbedd87f8215cfe8247b12d4e3ed3c
Shotizero <artem.alexeevich007@gmail.com>
2025-11-01 17:36:00 +0600

    Оптимизирован код и реализован парсинг (превращение string в int)

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,28 +1,78 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <string.h>
 
-int FindSlaeResult()
+const int ERROR = 0xFFFF;
+
+struct Equation
 {
-	char firstEquation[256], secondEquation[256], thirdEquation[256];
+	int x;
+	int y;
+	int z;
+	int Result;;
+};
 
-	printf("Enter first equation:\n");
-	scanf("%s", firstEquation);
+void InputEquation(std::string& buffer)
+{
+	std::getline(std::cin, buffer);
+}
 
-	printf("Enter second equation:\n");
-	scanf("%s", secondEquation);
+int ParseCoefficient(const std::string& coefficient)
+{
+	if (coefficient.empty())
+	{
+		printf("Error: empty coefficient");
+		return ERROR;
+	}
 
-	printf("Enter third equation:\n");
-	scanf("%s", thirdEquation);
+	if (coefficient == "+")
+	{
+		return 1;
+	}
 
-	printf("\nThe system of equations:\n%s\n%s\n%s\n\n", firstEquation, secondEquation, thirdEquation);
+	if (coefficient == "-")
+	{
+		return -1;
+	}
 
-	return 0;
+	return atoi(coefficient.c_str());
+}
+
+Equation ParseEquation(std::string& equationStr)
+{
+	Equation eq{};
+
+	if (equationStr.empty())
+	{
+		printf("Error: invalid equation!");
+		return eq;
+	}
+
+	std::string valueX = strtok(&equationStr[0], "Xx");
+	eq.x = ParseCoefficient(valueX);
+
+	std::string valueY = strtok(NULL, "Yy");
+	eq.y = ParseCoefficient(valueY);
+
+	std::string valueZ = strtok(NULL, "Zz");
+	eq.z = ParseCoefficient(valueZ);
+
+	std::string result = strtok(NULL, "=");
+	eq.Result = ParseCoefficient(result);
+
+	return eq;
 }
 
 int main()
 {
-	while (true)
+	Equation equations[3]{};
+
+	for(int i=0; i < 3; ++i)
 	{
-		FindSlaeResult();
+		std::string source;
+
+		InputEquation(source);
+		equations[i] = ParseEquation(source);
 	}
 
 	return 0;
