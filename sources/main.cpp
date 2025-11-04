f1a7ac61a475b517552d8da3e1ee49b0d40a99a6
Shotizero <artem.alexeevich007@gmail.com>
2025-11-04 20:23:30 +0600

    Реализовано циклическое решение систем уравнений и их вывод на консоль

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,10 +6,10 @@ const int ERROR = 0xFFFF;
 
 struct Equation
 {
-	int x;
-	int y;
-	int z;
-	int Result;;
+	int X;
+	int Y;
+	int Z;
+	int Result;
 };
 
 void InputEquation(std::string& buffer)
@@ -49,13 +49,13 @@ Equation ParseEquation(std::string& equationStr)
 	}
 
 	std::string valueX = strtok(&equationStr[0], "Xx");
-	eq.x = ParseCoefficient(valueX);
+	eq.X = ParseCoefficient(valueX);
 
 	std::string valueY = strtok(NULL, "Yy");
-	eq.y = ParseCoefficient(valueY);
+	eq.Y = ParseCoefficient(valueY);
 
 	std::string valueZ = strtok(NULL, "Zz");
-	eq.z = ParseCoefficient(valueZ);
+	eq.Z = ParseCoefficient(valueZ);
 
 	std::string result = strtok(NULL, "=");
 	eq.Result = ParseCoefficient(result);
@@ -63,18 +63,91 @@ Equation ParseEquation(std::string& equationStr)
 	return eq;
 }
 
-int main()
+void SolveEquations()
 {
+
 	Equation equations[3]{};
 
-	for(int i=0; i < 3; ++i)
+	for (int i = 0; i < 3; ++i)
 	{
 		std::string source;
+
+		std::string request[3]
+		{ { "Enter first equation" },
+		{ "\nEnter second equation" },
+		{ "\nEnter third equation" } };
+
+		std::cout<<request[i]<<"\n";
 
 		InputEquation(source);
 		equations[i] = ParseEquation(source);
 	}
 
+	int dX = (equations[0].Result * equations[1].Y * equations[2].Z) 
+		+ 
+		(equations[0].Y * equations[1].Z * equations[2].Result)
+		+ 
+		(equations[0].Z * equations[1].Result * equations[2].Y)
+		-
+		(equations[0].Z * equations[1].Y * equations[2].Result)
+		-
+		(equations[0].Result * equations[1].Z * equations[2].Y)
+		- 
+		(equations[0].Y * equations[1].Result * equations[2].Z);
+
+
+	int dY = (equations[1].Result * equations[0].X * equations[2].Z)
+		+ 
+		(equations[2].X * equations[1].Z * equations[0].Result)
+		+ 
+		(equations[0].Z * equations[2].Result * equations[1].X)
+		- 
+		(equations[0].Z * equations[2].X * equations[1].Result)
+		- 
+		(equations[2].Result * equations[0].X * equations[1].Z)
+		- 
+		(equations[1].X * equations[0].Result * equations[2].Z);
+
+
+	int dZ = (equations[2].Result * equations[0].X * equations[1].Y)
+		+ 
+		(equations[0].Y * equations[2].X * equations[1].Result)
+		+ 
+		(equations[1].X * equations[0].Result * equations[2].Y)
+		- 
+		(equations[1].Y * equations[2].X * equations[0].Result)
+		-
+		(equations[1].Result * equations[0].X * equations[2].Y)
+		-
+		(equations[0].Y * equations[2].Result * equations[1].X);
+
+
+	int D = (equations[0].X * equations[1].Y * equations[2].Z)
+		+
+		(equations[0].Y * equations[1].Z * equations[2].X)
+		+
+		(equations[0].Z * equations[1].X * equations[2].Y)
+		-
+		(equations[0].Z * equations[1].Y * equations[2].X)
+		-
+		(equations[0].Y * equations[1].X * equations[2].Z)
+		-
+		(equations[0].X * equations[1].Z * equations[2].Y);
+
+	int x = dX / D;
+	int y = dY / D;
+	int z = dZ / D;
+
+	printf("\nSolving a system of equations:\nX = %d\nY = %d\nZ = %d\n\n", x, y, z);
+
+}
+
+int main()
+{
+	while (true)
+	{
+		SolveEquations();
+	}
 	return 0;
 }
  
